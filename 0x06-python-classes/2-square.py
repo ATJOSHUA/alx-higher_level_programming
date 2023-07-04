592b6094bcc42376f00a8e55bc4b08b8edbf3213
Joshua <123208451+ATJOSHUA@users.noreply.github.com>
2023-07-04 10:04:16 +0100

    --------------->

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,13 +1,32 @@
 #!/usr/bin/python3
-class Square():
-    """class square.
-    Attributes:
-        __size: size of square.
+"""Class Square defines a square"""
+
+
+class Square:
+    """This class defines a square.
+
+    This class has no public attributes.
+
     """
     def __init__(self, size=0):
-        """Initialization, only allowed type int > 0."""
-        if type(size) != int:
+        """This method initiates a square.
+
+        Args:
+            size (int): This defines the size of the square.
+                The size is validated with try/except.
+
+        """
+        try:
+            self.__size = size
+            if size < 0:
+                raise ValueError
+            if type(size) is not int:
+                raise TypeError
+        except TypeError:
             raise TypeError("size must be an integer")
-        if (int(size) < 0):
+        except ValueError:
             raise ValueError("size must be >= 0")
-        self.__size = size
+
+    def area(self):
+        """int: Return area of square."""
+        return self.__size * self.__size
