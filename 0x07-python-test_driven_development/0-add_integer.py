45985f7a1f149327c272c3cd2c04f2a9cefb0a56
Joshua <123208451+ATJOSHUA@users.noreply.github.com>
2023-07-06 00:04:36 +0100

    Update 0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
 
-"""contains a function to add two integers"""
 
 def add_integer(a, b=98):
-
-    """function that returns an integer
-    args:
-        a (int or float): first param
-        b (int or float): second param
-    Raises TypeError with corresponding message if a or b
-    isn't an int or a float
     """
-    if type(a) not in [int, float]:
-        raise TypeError("a must be an integer")
-    if type(b) not in [int, float]:
-        raise TypeError("b must be an integer")
-    return int(a) + int(b)
+    adds an integer
+    unit tests located in tests/0-add_integer.txt
+    checks for type errors
+    """
+    if not isinstance(a, int):
+        if isinstance(a, float):
+            a = int(a)
+        else:
+            raise TypeError("a must be an integer")
+    if not isinstance(b, int):
+        if isinstance(b, float):
+            b = int(b)
+        else:
+            raise TypeError("b must be an integer")
+    return a + b
