34ac9273f739102054e61d3abed46cda667c6c59
Joshua <123208451+ATJOSHUA@users.noreply.github.com>
2023-07-11 02:34:53 +0100

    -------)------------(-

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 
+"""MyInt class that inherits from int"""
+
 
 class MyInt(int):
-    """
-    miint
-    """
-    def __eq__(self, other):
-        return int(self) != int(other)
+    """MyInt class with != and == interchanged"""
+    def __eq__(self, value):
+        """Override == opeartor with != behavior."""
+        return self.real != value
 
-    def __ne__(self, other):
-        return int(self) == int(other)
+    def __ne__(self, value):
+        """Override != operator with == behavior."""
+        return self.real == value
