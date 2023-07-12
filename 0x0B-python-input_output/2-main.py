2297f8372fc6d270b438666929e8859f8d0ec7ac
Joshua <123208451+ATJOSHUA@users.noreply.github.com>
2023-07-13 00:15:12 +0100

    ---------------------------------<

--- 0x0B-python-input_output/2-main.py
+++ 0x0B-python-input_output/2-main.py
@@ -1,11 +1,29 @@
 #!/usr/bin/python3
-read_lines = __import__('2-read_lines').read_lines
+"""Defines a text file-reading function."""
 
-print("1 line:")
-read_lines("my_file_0.txt", 1)
-print("--")
-print("3 lines:")
-read_lines("my_file_0.txt", 3)
-print("--")
-print("Full content:")
-read_lines("my_file_0.txt")
+
+def read_lines(filename="", nb_lines=0):
+    """Print a given number of lines from a UTF8 text file to stdout.
+
+    Args:
+        filename (str): The name of the file.
+        nb_lines (int): The number of lines to read from the file.
+    """
+    with open(filename, encoding="utf-8") as f:
+        if nb_lines <= 0:
+            print(f.read(), end="")
+            return
+
+        lines = 0
+        for line in f:
+            lines += 1
+        f.seek(0)
+        if nb_lines >= lines:
+            print(f.read(), end="")
+            return
+
+        else:
+            n = 0
+            while n < nb_lines:
+                print(f.readline(), end="")
+                n += 1
