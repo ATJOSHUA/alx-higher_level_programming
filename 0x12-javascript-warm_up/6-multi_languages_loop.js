3bce37167e21248e448a6f77a7961b78fa36f14e
Joshua <123208451+ATJOSHUA@users.noreply.github.com>
2023-12-13 11:22:13 +0100

    [RY]

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
-const words = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (const i in words) {
-  console.log(words[i]);
-}
+// Graham S. Paul (6-multi_languages_loop.js)
+const data = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+
+data.forEach(line => console.log(line));
