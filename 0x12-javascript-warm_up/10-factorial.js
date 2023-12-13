a3cf5ed211e7412956054f2fe383396dbadfecd4
Joshua <123208451+ATJOSHUA@users.noreply.github.com>
2023-12-13 11:10:14 +0100

    [RY]

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,15 +1,11 @@
 #!/usr/bin/node
-const array = process.argv.slice(2);
-const number = parseInt(array[0]);
-if (isNaN(array[0])) {
-  console.log(1);
-} else {
-  console.log(factorial(number));
-}
+// Graham S. Paul (10-factorial.js)
 function factorial (n) {
-  if (n === 0) {
+  if ((isNaN(n)) || (n === 1)) {
     return 1;
   } else {
     return n * factorial(n - 1);
   }
 }
+
+console.log(factorial(parseInt(process.argv[2])));
