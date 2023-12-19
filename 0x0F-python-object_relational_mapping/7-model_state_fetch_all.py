089738294372f425b8d472b7ddfc59e9094f31de
Joshua <123208451+ATJOSHUA@users.noreply.github.com>
2023-12-19 20:43:22 +0100

    Update 7-model_state_fetch_all.py

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,24 +1,32 @@
 #!/usr/bin/python3
-"""
-module 7
-"""
-import sys
-from model_state import Base, State
 
-from sqlalchemy import (create_engine)
+"""
+Module to perfom simple queries on the model_state model
+using and ORM - SQLAlchemy
+"""
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+
+def connect_and_query(user: str, passwd: str, dbase: str) -> None:
+
+    """
+    Connect to the database and make queries using ORM
+    """
+    try:
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                               .format(user, passwd, dbase))
+        Session = sessionmaker(bind=engine)
+        state_session = Session()
+        states = state_session.query(State).order_by(State.id).all()
+
+        for state in states:
+            print("{}: {}".format(state.id, state.name))
+    except Exception as e:
+        return e
+
 
 if __name__ == "__main__":
-    cont = 0
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-    q = engine.execute("SELECT * FROM states ORDER BY states.id ASC")
-    content = q.fetchall()
-    for iter in content:
-        for iter2 in iter:
-            if cont > 0:
-                print(": ", end='')
-            print(iter2, end='')
-            cont = cont + 1
-        cont = 0
-        print()
+    connect_and_query(sys.argv[1], sys.argv[2], sys.argv[3])
