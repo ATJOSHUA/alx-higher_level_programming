9e6e258c91c3602e7613627ad3b67f2b20698811
Joshua <123208451+ATJOSHUA@users.noreply.github.com>
2024-01-08 18:44:18 +0100

    Update 8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
-# task4
-import requests
+"""Sends a POST request to http://0.0.0.0:5000/search_user with a given letter.
+Usage: ./8-json_api.py <letter>
+  - The letter is sent as the value of the variable `q`.
+  - If no letter is provided, sends `q=""`.
+"""
 import sys
+import requests
 
-if __name__ == '__main__':
-    url = 'http://0.0.0.0:5000/search_user'
-    if len(sys.argv) > 1:
-        q = sys.argv[1]
-    else:
-        q = ""
-    value = {'q': q}
-    req = requests.post(url, data=value)
+
+if __name__ == "__main__":
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    payload = {"q": letter}
+
+    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
     try:
-        if not req.json():
+        response = r.json()
+        if response == {}:
             print("No result")
         else:
-            print("[{}] {}".format(
-                req.json().get('id'),
-                req.json().get('name')))
+            print("[{}] {}".format(response.get("id"), response.get("name")))
     except ValueError:
         print("Not a valid JSON")
