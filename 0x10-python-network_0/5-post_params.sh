970ac7cfe6aada8027a272231ffc69de127e6127
Joshua <123208451+ATJOSHUA@users.noreply.github.com>
2024-01-08 18:07:20 +0100

    5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# 5- curl post
-curl -s --request POST "$1" -d 'email=hr@holbertonschool.com&subject=I will always be here for PLD'
+# Take in URL, POST key:vals; Usage: ./5-post_params.sh 0.0.0.0:5000/route_6 ; echo ""
+curl -s -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
