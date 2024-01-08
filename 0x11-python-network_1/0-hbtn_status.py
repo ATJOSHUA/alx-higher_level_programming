55de38d7154f40a9045ed806b4829649ea613dee
Joshua <123208451+ATJOSHUA@users.noreply.github.com>
2024-01-08 18:29:22 +0100

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-# task 0
+"""Fetches https://alx-intranet.hbtn.io/status."""
 import urllib.request
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-    html = response.read()
-print("Body response:")
-print("\t- type: {}".format(type(html)))
-print("\t- content: {}".format(html))
-print("\t- utf8 content: {}".format(html.decode('utf-8')))
+
+
+if __name__ == "__main__":
+    req = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(req) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
