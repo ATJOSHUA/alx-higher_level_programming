349ed1dc9f9716e2401ece1e9e0aa31e262052d6
Joshua <123208451+ATJOSHUA@users.noreply.github.com>
2024-01-08 18:36:03 +0100

    100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
-# task adavance 11
+"""Lists the 10 most recent commits on a given GitHub repository.
+Usage: ./100-github_commits.py <repository name> <repository owner>
+"""
 import sys
 import requests
 
-if __name__ == '__main__':
-    url = 'https://api.github.com/repos/{}/{}/commits'.format(sys.argv[2],
-                                                              sys.argv[1])
-    req = requests.get(url)
-    content = req.json()
-    cont = 0
-    for item in content:
-        if cont < 10:
-            print(item['sha'] + ': ' + item['commit']['author']['name'])
-            cont += 1
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
+
+    r = requests.get(url)
+    commits = r.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
