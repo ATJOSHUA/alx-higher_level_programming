8b7d128206e72d72214afa6a12d32bf6cd4b23c0
Joshua <123208451+ATJOSHUA@users.noreply.github.com>
2024-01-08 18:42:08 +0100

    Update 6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
-# task6
-import requests
+"""Sends a POST request to a given URL with a given email.
+Usage: ./6-post_email.py <URL> <email>
+  - Displays the body of the response.
+"""
 import sys
+import requests
 
-if __name__ == '__main__':
-    value = {'email': sys.argv[2]}
-    r = requests.post(sys.argv[1], data=value)
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+
+    r = requests.post(url, data=value)
     print(r.text)
